2717afb4bf899733333c42e91c3d547fc88ab289
Seth Bromberger <github@bromberger.com>
2025-12-11 09:57:16 -0800

    stderr always prints, remove debug statement. Closes #24 and #25

--- src/clippy/backends/fs/execution.py
+++ src/clippy/backends/fs/execution.py
@@ -106,7 +106,8 @@ def _stream_exec(
     if not d:
         return None, stderr
     if stderr:
-        logger.debug('Received stderr: %s', stderr)
+        print(stderr)
+        # logger.debug('Received stderr: %s', stderr)
     logger.debug('run(): final stdout = %s', d)
 
     return (d, stderr)
