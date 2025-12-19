3d9a617bcde7d47b854aa5aa33e5781a62476d8d
Seth Bromberger <github@bromberger.com>
2025-12-19 22:19:59 +0000

    not working

--- py/src/clippy/selectors.py
+++ py/src/clippy/selectors.py
@@ -13,51 +13,51 @@ class Selector(jl.Variable):
 
     def __init__(self, parent: Selector | None, name: str, docstr: str):
         super().__init__(name, docstr)  # op and o2 are None to represent this as a variable.
-        self.parent = parent
-        self.name = name
-        self.fullname: str = self.name if self.parent is None else f"{self.parent.fullname}.{self.name}"
-        self.subselectors: set[Selector] = set()
+        self._parent = parent
+        self._name = name
+        self._fullname: str = self._name if self._parent is None else f"{self._parent._fullname}.{self._name}"
+        self._subselectors: set[Selector] = set()
 
     def __hash__(self):
-        return hash(self.fullname)
+        return hash(self._fullname)
 
-    def prepare(self):
-        return {"var": self.fullname}
+    def _prepare(self):
+        return {"var": self._fullname}
 
-    def hierarchy(self, acc: list[tuple[str, str]] | None = None):
+    def _hierarchy(self, acc: list[tuple[str, str]] | None = None):
         if acc is None:
             acc = []
-        acc.append((self.fullname, self.__doc__ or ""))
-        for subsel in self.subselectors:
-            subsel.hierarchy(acc)
+        acc.append((self._fullname, self.__doc__ or ""))
+        for subsel in self._subselectors:
+            subsel._hierarchy(acc)
         return acc
 
-    def describe(self):
-        hier = self.hierarchy()
+    def _describe(self):
+        hier = self._hierarchy()
         maxlen = max(len(sub_desc[0]) for sub_desc in hier)
         return "\n".join(f"{sub_desc[0]:<{maxlen + 2}} {sub_desc[1]}" for sub_desc in hier)
 
     def __str__(self):
-        return repr(self.prepare())
+        return repr(self._prepare())
 
-    def to_serial(self):
-        return {"var": self.fullname}
+    def _to_serial(self):
+        return {"var": self._fullname}
 
     def _add_subselector(self, name: str, docstr: str):
         """add a subselector to this selector"""
         subsel = Selector(self, name, docstr)
         setattr(self, name, subsel)
-        self.subselectors.add(subsel)
+        self._subselectors.add(subsel)
 
     def _del_subselector(self, name: str):
         delattr(self, name)
-        self.subselectors.remove(getattr(self, name))
+        self._subselectors.remove(getattr(self, name))
 
     def _clear_subselectors(self):
         """removes all subselectors"""
-        for subsel in self.subselectors:
-            delattr(self, subsel.name)
-        self.subselectors = set()
+        for subsel in self._subselectors:
+            delattr(self, subsel._name)
+        self._subselectors = set()
 
     def _import_from_dict(self, d: AnyDict, merge: bool = False):
         """Imports subselectors from a dictionary.
